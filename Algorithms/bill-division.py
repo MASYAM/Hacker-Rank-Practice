934e5d7a9e71510378cd4d399ea1277733723036
Md Abdul Awal <masyam.cse@gmail.com>
2020-10-15 20:56:55 +0200

    Create bill-division.py

---
+++ Algorithms/bill-division.py
@@ -0,0 +1,34 @@
+#https://www.hackerrank.com/challenges/bon-appetit/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    count=0
+    for i in range(len(bill)):
+        if(i!=k):
+            count+=bill[i]
+    actual_pay = count//2
+    if(actual_pay != b):
+        print(b-actual_pay)
+    else:
+        print("Bon Appetit")
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
